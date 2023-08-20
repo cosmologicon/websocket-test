e477e30372ed8d8c6776b09f95ba18a04177a1fe
Christopher Night <cosmologicon@gmail.com>
2023-08-20 16:11:25 -0400

    Server v2: remove a bunch of unused code

--- server-v2.py
+++ server-v2.py
@@ -15,6 +15,7 @@ import socketserver
 import asyncio
 import hashlib
 import base64
+from io import BytesIO
 
 PORT = 1234
 assert PORT > 1023  # Required if running as unprivileged user.
@@ -51,53 +52,6 @@ assert joinbits((0, 2), (10, 4), (2, 2)) == 42
 
 
 
-
-class WebSocketHandler(http.server.BaseHTTPRequestHandler):
-	SALT = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-	# Produces the Sec-WebSocket-Accept string for the Server handshake response.
-	# https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#server_handshake_response
-	# key: the Sec-Websocket-Key provided by the client.
-	def get_accept(self, key: str) -> str:
-		digest = hashlib.sha1(key.encode("utf-8") + self.SALT).digest()
-		return base64.b64encode(digest).decode("utf-8")
-
-	# https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#format
-	def extract_frame(self, rfile):
-		b0 = pullbytes(rfile, 2)
-		FIN, RSV, opcode, MASK, payload_len = splitbits(b0, 1, 3, 4, 1, 7)
-		print("extract_frame", FIN, RSV, opcode, MASK, payload_len)
-		# Not supported in this version:
-		# FIN = 0 and opcode = 0: message fragmentation.
-		# RSV > 0: extensions.
-		# opcode = 2: binary data.
-		# opcode = 9, 10: ping/pong.
-		assert (FIN, RSV, MASK) == (1, 0, 1)
-		assert opcode in [1, 8]
-		is_close = opcode == 8
-		if payload_len == 126:
-			payload_len = pullbytes(rfile, 2)
-		elif payload_len == 127:
-			payload_len = pullbytes(rfile, 8)
-		print("extract_frame fields", FIN, RSV, opcode, MASK, payload_len)
-		mask_key = [pullbytes(rfile, 1) for _ in range(4)]
-		# Future improvement:
-		# https://stackoverflow.com/questions/46540337/python-xoring-each-byte-in-bytes-in-the-most-efficient-way
-		ENCODED = [pullbytes(rfile, 1) for _ in range(payload_len)]
-		payload = "".join(chr(char ^ mask_key[j % 4]) for j, char in enumerate(ENCODED))
-		return is_close, payload
-		
-
-	def encode_frame(self, text: str, opcode: int = 1) -> bytes:
-		frame = []
-		assert opcode in [1, 8]
-		FIN, RSV, MASK, payload_len = 1, 0, 0, len(text)
-		assert payload_len < 126
-		frame.append(joinbits((FIN, 1), (RSV, 3), (opcode, 4)).to_bytes(1, "big"))
-		frame.append(joinbits((MASK, 1), (payload_len, 7)).to_bytes(1, "big"))
-		frame.append(text.encode("utf-8"))
-		return b"".join(frame)
-
-
 class ClientTracker:
 	def __init__(self):
 		self.nclient = 0
@@ -108,64 +62,11 @@ class ClientTracker:
 client_tracker = ClientTracker()
 
 
-class Handler(WebSocketHandler):
-	def __init__(self, request, client_address, server):
-		self.client_id = client_tracker.get_client_id()
-		print(f"STARTING __init__ for client {self.client_id}")
-		print("  ARG request:", request)
-		print("  ARG client_address:", client_address)
-		print("  ARG server:", server)
-		super().__init__(request, client_address, server)
-		print(f"ENDING __init__ for client {self.client_id}")
-
-	def do_GET(self):
-		print("***** GET HANDLER *****")
-		print("client address:", self.client_address)
-		print("command:", self.command)
-		print("path:", self.path)
-		print("request_version:", self.request_version)
-		print("server_version:", self.server_version)
-		print("sys_version:", self.sys_version)
-		print("address_string:", self.address_string())
-		for key, value in self.headers.items():
-			print("header:", key, value)
-		
-		if "Sec-WebSocket-Version" not in self.headers:
-			self.send_response(400, "Bad Request")
-			return
-			
-		print("***** HANDSHAKE *****")
-		self.send_response(101, "Switching Protocols")
-		self.send_header("Upgrade", "websocket")
-		self.send_header("Connection", "Upgrade")
-		accept = self.get_accept(self.headers["Sec-WebSocket-Key"])
-		print("accept:", accept)
-		self.send_header("Sec-WebSocket-Accept", accept)
-		self.end_headers()
-		print("Handshake complete. Awaiting payload.")
-
-		for nmessage in range(5):
-			is_close, payload = self.extract_frame(self.rfile)
-			print("***** FRAME RECEIVED *****")
-			print("Is close:", is_close)
-			print("Payload:", payload)
-			if is_close:
-				break
-			response = f"Client #{self.client_id} payload #{nmessage}: {payload}"
-			print("Sending response:", response)
-			self.wfile.write(self.encode_frame(response))
-			if payload == "CLOSE":
-				break
-		print("SENDING CLOSE FRAME")
-		print()
-		self.wfile.write(self.encode_frame("", opcode = 8))
-
-
-SALT = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 # Produces the Sec-WebSocket-Accept string for the Server handshake response.
 # https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#server_handshake_response
 # key: the Sec-Websocket-Key provided by the client.
 def get_accept(key: str) -> str:
+	SALT = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 	digest = hashlib.sha1(key.encode("utf-8") + SALT).digest()
 	return base64.b64encode(digest).decode("utf-8")
 
@@ -204,16 +105,8 @@ def encode_frame(text: str, opcode: int = 1) -> bytes:
 	frame.append(text.encode("utf-8"))
 	return b"".join(frame)
 
-
-from io import BytesIO
-class Handler2(Handler):
-	def __init__(self, request_text):
-		self.rfile = BytesIO(request_text)
-		self.raw_requestline = self.rfile.readline()
-		self.error_code = self.error_message = None
-		self.parse_request()
-
-class HTTPRequest(http.server.BaseHTTPRequestHandler):
+# We don't actually use the BaseHTTPRequestHandler class for anything except to parse the headers.
+class HTTPRequestParser(http.server.BaseHTTPRequestHandler):
 	def __init__(self, request_text):
 		self.rfile = BytesIO(request_text)
 		self.raw_requestline = self.rfile.readline()
@@ -237,7 +130,7 @@ async def send_http(stream_writer, status_code, status_text, headers = ()):
 	print("send_http", message)
 	stream_writer.write(message)
 	await stream_writer.drain()
-	
+
 
 async def handle_GET(client_id, request, stream_reader, stream_writer):
 	print("***** GET HANDLER *****")
@@ -284,32 +177,20 @@ async def handle_GET(client_id, request, stream_reader, stream_writer):
 	print()
 	stream_writer.write(encode_frame("", opcode = 8))
 	await stream_writer.drain()
+	stream_writer.close()
+	await stream_writer.wait_closed()
 
 
-
-def main():
-	with socketserver.TCPServer(("", PORT), Handler) as httpd:
-		print("serving at port", PORT)
-		httpd.serve_forever()
-if False:
-	main()
-
-
-server = None
 async def handle(stream_reader, stream_writer):
 	request_text = await stream_reader.readuntil(b"\r\n\r\n")
-	print("REQUEST0", request_text)
-	request = HTTPRequest(request_text)
-	print(request)
+	request = HTTPRequestParser(request_text)
 	await handle_GET(client_tracker.get_client_id(), request, stream_reader, stream_writer)
 
-
+server = None
 async def run_server():
 	global server
 	server = await asyncio.start_server(handle, host="", port=PORT)
 	async with server:
 		await server.serve_forever()
-
-if True:
-	asyncio.run(run_server())
+asyncio.run(run_server())
 
